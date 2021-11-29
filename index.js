bb5085c5857fd322aa91350ec0f13d7d51491fde
Gavin Fu <gavfu@outlook.com>
2021-11-29 14:20:51 +0800

    Use IPFS.addAll and update action info

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ async function main() {
         }
     });
 
-    const { cid } = await ipfs.add(globSource(path, { recursive: true }));
+    const { cid } = await ipfs.addAll(globSource(path, { recursive: true }));
 
     if (cid) {
         core.setOutput('hash', cid.toV0().toString());
